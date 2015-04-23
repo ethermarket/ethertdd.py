e1e2b51b270f981dbb75f2e52a7f865e1ad8d8a0
Ryan Casey <ryepdx@gmail.com>
2015-04-22 17:58:04 -0700

    Update example contract to PoC 9.

--- contracts/example.sol
+++ contracts/example.sol
@@ -1,14 +1,14 @@
 contract example{
     address public owner;
     uint public fee;
-    mapping (string32 => address) public names;
+    mapping (bytes32 => address) public names;
 
     function example() {
         owner = msg.sender;
         fee = 10000; // in wei
     }
 
-    function setName(string32 name, address newAddress) returns (bool r) {
+    function setName(bytes32 name, address newAddress) returns (bool r) {
         if (!isOwner() || msg.value < fee || (names[name] != 0x0 && names[name] != msg.sender)) {
             return false;
         }
